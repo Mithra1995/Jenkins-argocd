d9dfe472932c4ddefce1fc8df261d7cfce5993d4
Mithra1995 <mithra.balasubramaniam05@gmail.com>
2025-07-16 19:49:21 -0400

    Update StartApplicationTests.java

--- spring-boot-app/src/test/java/com/abhishek/StartApplicationTests.java
+++ spring-boot-app/src/test/java/com/abhishek/StartApplicationTests.java
@@ -1,1 +1,27 @@
+package com.abhishek;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.ModelMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class StartApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Basic Spring context test
+    }
+
+    @Test
+    void testIndexViewName() {
+        StartApplication app = new StartApplication();
+        ModelMap model = new ModelMap();
+        String view = app.index(model);
+
+        assertEquals("index", view);
+        assertEquals("I have successfuly built a sprint boot application using Maven", model.get("title"));
+        assertEquals("This application is deployed on to Kubernetes using Argo CD", model.get("msg"));
+    }
+}
