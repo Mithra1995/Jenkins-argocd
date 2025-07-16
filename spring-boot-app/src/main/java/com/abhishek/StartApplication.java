4be7b53ed9c930a84e5e3aecba84f5b4ab58e987
Mithra1995 <mithra.balasubramaniam05@gmail.com>
2025-07-16 19:40:20 -0400

    Update StartApplication.java

--- spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -1,24 +1,27 @@
 package com.abhishek;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.ModelMap;
 
-@SpringBootApplication
-@Controller
-public class StartApplication {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    @GetMapping("/")
-    public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
-        return "index";
+@SpringBootTest
+class StartApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Just tests if the Spring context loads successfully
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
-    }
+    @Test
+    void testIndexViewName() {
+        StartApplication app = new StartApplication();
+        ModelMap model = new ModelMap();
+        String viewName = app.index(model);
 
+        assertEquals("index", viewName);
+        assertEquals("I have successfuly built a sprint boot application using Maven", model.get("title"));
+        assertEquals("This application is deployed on to Kubernetes using Argo CD", model.get("msg"));
+    }
 }
